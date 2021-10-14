32e7b174d017844fe790540fc0915198261400ba
Conor McGovern <c.mcgovern014@gmail.com>
2021-10-14 17:50:25 -0600

    Move the search page into seperate component

--- src/App.js
+++ src/App.js
@@ -5,29 +5,13 @@ import {
   Toolbar,
   Typography,
   Grid,
-  InputLabel,
-  useMediaQuery,
   CssBaseline,
 } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import SearchWidget from './components/SearchWidget';
-import SortSelect from './components/SortSelect';
-import LanguageSelect from './components/LanguageSelect';
-import LanguageMenu from './components/LanguageMenu';
-import Results from './components/Results';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import RespositoryDetails from './components/RepositoryDetails';
-
-function ResultsHeader() {
-  return (
-    <Typography variant="h6">Results for &quot;react query&quot;</Typography>
-  );
-}
+import RespositoryDetails from './views/RepositoryDetails';
+import Search from './views/Search';
 
 function App() {
-  const theme = useTheme();
-  const isExtraSmallWidth = useMediaQuery(theme.breakpoints.only('xs'));
-
   return (
     <Router>
       <CssBaseline />
@@ -40,55 +24,7 @@ function App() {
         <Grid container spacing={2} sx={{ paddingTop: '2rem' }}>
           <Switch>
             <Route exact path="/">
-              <Grid item xs={12}>
-                <SearchWidget />
-              </Grid>
-              {isExtraSmallWidth && (
-                <>
-                  <Grid item xs={12}>
-                    <ResultsHeader />
-                  </Grid>
-                  <Grid item container spacing={2} alignItems="center">
-                    <Grid item>
-                      <InputLabel sx={{ minWidth: '80px' }}>
-                        Language
-                      </InputLabel>
-                    </Grid>
-                    <Grid item flexGrow={1}>
-                      <LanguageSelect />
-                    </Grid>
-                  </Grid>
-                  <Grid item container spacing={2} alignItems="center">
-                    <Grid item>
-                      <InputLabel sx={{ minWidth: '80px' }}>Sort</InputLabel>
-                    </Grid>
-                    <Grid item flexGrow={1}>
-                      <SortSelect />
-                    </Grid>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Results />
-                  </Grid>
-                </>
-              )}
-              {!isExtraSmallWidth && (
-                <>
-                  <Grid item xs={3}>
-                    <LanguageMenu />
-                  </Grid>
-                  <Grid container item xs={9}>
-                    <Grid item>
-                      <ResultsHeader />
-                    </Grid>
-                    <Grid item sx={{ marginLeft: 'auto' }}>
-                      <SortSelect renderValue={(value) => `Sort: ${value}`} />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <Results />
-                    </Grid>
-                  </Grid>
-                </>
-              )}
+              <Search />
             </Route>
             <Route path="/:ownerLogin/:repoName">
               <RespositoryDetails />
---
+++ src/views/Search.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Grid, InputLabel, Typography, useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
+import SearchWidget from '../components/SearchWidget';
+import SortSelect from '../components/SortSelect';
+import LanguageSelect from '../components/LanguageSelect';
+import LanguageMenu from '../components/LanguageMenu';
+import Results from '../components/Results';
+
+function ResultsHeader() {
+  return (
+    <Typography variant="h6">Results for &quot;react query&quot;</Typography>
+  );
+}
+
+function Search() {
+  const theme = useTheme();
+  const isExtraSmallWidth = useMediaQuery(theme.breakpoints.only('xs'));
+  return (
+    <>
+      <Grid item xs={12}>
+        <SearchWidget />
+      </Grid>
+      {isExtraSmallWidth && (
+        <>
+          <Grid item xs={12}>
+            <ResultsHeader />
+          </Grid>
+          <Grid item container spacing={2} alignItems="center">
+            <Grid item>
+              <InputLabel sx={{ minWidth: '80px' }}>Language</InputLabel>
+            </Grid>
+            <Grid item flexGrow={1}>
+              <LanguageSelect />
+            </Grid>
+          </Grid>
+          <Grid item container spacing={2} alignItems="center">
+            <Grid item>
+              <InputLabel sx={{ minWidth: '80px' }}>Sort</InputLabel>
+            </Grid>
+            <Grid item flexGrow={1}>
+              <SortSelect />
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Results />
+          </Grid>
+        </>
+      )}
+      {!isExtraSmallWidth && (
+        <>
+          <Grid item xs={3}>
+            <LanguageMenu />
+          </Grid>
+          <Grid container item xs={9}>
+            <Grid item>
+              <ResultsHeader />
+            </Grid>
+            <Grid item sx={{ marginLeft: 'auto' }}>
+              <SortSelect renderValue={(value) => `Sort: ${value}`} />
+            </Grid>
+            <Grid item xs={12}>
+              <Results />
+            </Grid>
+          </Grid>
+        </>
+      )}
+    </>
+  );
+}
+
+export default Search;
