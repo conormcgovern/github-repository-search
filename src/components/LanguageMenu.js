b64810cd6eef8f9a7aa232e5d6bcf4bd89811012
Conor McGovern <c.mcgovern014@gmail.com>
2021-10-15 14:51:21 -0600

    render links in the language menu

--- src/components/LanguageMenu.js
+++ src/components/LanguageMenu.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { Paper, List, ListItem, ListItemText, Divider } from '@mui/material';
+import { Link, useLocation } from 'react-router-dom';
+
+const options = [
+  { value: 'javascript', display: 'JavaScript' },
+  { value: 'typescript', display: 'TypeScript' },
+  { value: 'html', display: 'HTML' },
+  { value: 'css', display: 'CSS' },
+  { value: 'java', display: 'Java' },
+];
 
 function LanguageMenu() {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  searchParams.delete('language');
+
   return (
     <Paper elevation={0} sx={{ height: '100%', border: '1px solid #DDDDDD' }}>
       <List>
@@ -9,21 +22,13 @@ function LanguageMenu() {
           <ListItemText primary="Languages"></ListItemText>
         </ListItem>
         <Divider />
-        <ListItem>
-          <ListItemText secondary="JavaScript"></ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText secondary="TypeScript"></ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText secondary="HTML"></ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText secondary="CSS"></ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText secondary="Java"></ListItemText>
-        </ListItem>
+        {options.map((option) => (
+          <ListItem key={option.value}>
+            <Link to={`search?${searchParams}&language=${option.value}`}>
+              <ListItemText secondary={option.display}></ListItemText>
+            </Link>
+          </ListItem>
+        ))}
       </List>
     </Paper>
   );
